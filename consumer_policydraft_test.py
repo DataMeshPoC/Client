efcfaf03cb69b0b29d46e8c2124ad0e881584a66
Mikanribu <clementine.pages@gmail.com>
2022-07-19 17:11:15 +0800

    consumer policy draft list example

---
+++ consumer_policydraft_test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import uuid  # for consumer group
+from confluent_kafka import Consumer, KafkaError, KafkaException
+import struct
+
+from confluent_avro import AvroKeyValueSerde, SchemaRegistry
+from confluent_avro.schema_registry import HTTPBasicAuth
+from confluent_avro import AvroValueSerde
+
+
+# for debugging
+import traceback
+
+
+# mainly taken from https://docs.confluent.io/kafka-clients/python/current/overview.html#id1
+def basic_consume_loop(consumer, topics, avroSerde):
+    try:
+        consumer.subscribe(topics)
+
+        while True:
+            msg = consumer.poll(10)
+            if msg is None:
+                continue
+            if msg.error():
+                print('Consumer error: {}'.format(msg.error()))
+                continue
+            else:
+                # using avro parser here
+                if msg.value() is not None:
+                    v = avroSerde.deserialize(msg.value())
+                    k = struct.unpack('>i', msg.key())[0]
+                    print('>> {} {} {} {}'.format(msg.topic(), msg.partition(), msg.offset(), k))
+                    print('Consumed: {}'.format(v))
+                    print('\n')
+    finally:
+        consumer.close()
+
+
+def main():
+    consumer = Consumer({
+        'bootstrap.servers': 'pkc-epwny.eastus.azure.confluent.cloud:9092',
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': 'IHO7XVPCJCCBZAYX',
+        'sasl.password': 'UAwjmSIn5xuAL7HZmBjU4NGt0nLfXbyjtlVA7imgCdGBYFkog5kw0gc4e5MYmiUE',
+        'group.id': str(uuid.uuid1()),  # just generating a groupid, can be replaced by a specific one
+        'auto.offset.reset': 'earliest'
+    })
+
+    # topic name used by parser
+    KAFKA_TOPIC = "PolicyDraftList"
+
+    registry_client = SchemaRegistry(
+        "https://psrc-gq7pv.westus2.azure.confluent.cloud",
+        HTTPBasicAuth("MYXDIGGTQEEMLDU2", "azvNIgZyA4TAaOmCLzxvrXqDpaC+lamOvkGm2B7mdYrq9AwKl4IQuUq9Q6WXOp8U"),
+        headers={"Content-Type": "application/vnd.schemaregistry.v1+json"},
+    )
+
+    avroSerde = AvroValueSerde(registry_client, KAFKA_TOPIC)
+
+    basic_consume_loop(consumer, [KAFKA_TOPIC], avroSerde)
+
+
+if __name__ == '__main__':
+    try:
+        input("Press Enter to start")
+        main()
+    except Exception:
+        # for debugging
+        print(traceback.format_exc())
+        input("Press return to exit")
