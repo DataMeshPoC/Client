39895b5a5f10b6dfc3ef926878be4380bf1df642
Mikanribu <clementine.pages@gmail.com>
2022-07-20 17:14:52 +0800

    update producer part, update index with form

--- app.py
+++ app.py
@@ -1,46 +1,21 @@
-from datetime import datetime
 import os
-import sys
-import pathlib
-import subprocess
-import email
-import io 
-import consumer_policydraft_test as c
-import pyodbc
-import producer_policy_uw_result
-import producer_policy
-import numpy as np 
-import pysftp
-import pandas as pd
-import argparse
 import logging
-import stat
-from pathlib import Path
-from multiprocessing import pool
-from codecs import getencoder
-from distutils.sysconfig import customize_compiler
-from unicodedata import name
 from threading import Thread, Event
-from queue import Queue
-from json import dumps
-from pytz import country_names
-from queue import Queue
-import uuid
-import avro.schema
-import avro.io
 import uuid  # for consumer group
-from confluent_kafka import Consumer, KafkaError, KafkaException
 import struct
-# for debugging
-import traceback
-from confluent_kafka import Consumer, KafkaError, KafkaException
-from confluent_avro import AvroKeyValueSerde, SchemaRegistry, AvroValueSerde
+import datetime
+
+from confluent_kafka import Consumer, SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import IntegerSerializer
+from confluent_avro import SchemaRegistry, AvroValueSerde
 from confluent_avro.schema_registry import HTTPBasicAuth
+
 from helpers import login_required, apology
-from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.exceptions import HTTPException, InternalServerError
 
 # Configure application
 app = Flask(__name__, template_folder='templates')
@@ -64,6 +39,7 @@ if __name__ == "__main__":
     app.debug = True
     app.run()
 
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -71,6 +47,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -104,6 +81,7 @@ def login():
     else:
         return render_template("login.html")
 
+
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -113,6 +91,7 @@ def logout():
 
     # Redirect user to login form
     return redirect("/login")
+
 
 @app.route("/", methods=["GET", "POST"])
 @login_required
@@ -138,9 +117,10 @@ def index():
                             v = avroSerde.deserialize(msg.value())
                             k = struct.unpack('>i', msg.key())[0]
                             print(v)
-                            return(v)
+                            return v
             finally:
                 consumer.close()
+
         def client_consumed():
             # topic name used by parser
             KAFKA_TOPIC = "PolicyDraftList"
@@ -162,6 +142,7 @@ def index():
             return basic_consume_loop(consumer, ["PolicyDraftList"], avroSerde)
         
         # Store the dict from the consumer call
+        """
         v = client_consumed()
         print(v)
         # Index into dict for each entry to be rendered
@@ -183,6 +164,7 @@ def index():
         POLICYTERM = v['POLICYTERM']
         POLICYDESCRIPTION = v['POLICYDESCRIPTION']
         DOB = v['DOB']
+        """
 
         # producer configs 
         def acked(err, msg):
@@ -191,100 +173,95 @@ def index():
             else:
                 print('Produced to: {} [{}] @ {}'.format(msg.topic(), msg.partition(), msg.offset()))
 
+        def producer(uw_result, status):
 
-        def producer():
-            
-            """
-            producer = Producer({
-                'bootstrap.servers': 'pkc-epwny.eastus.azure.confluent.cloud:9092',
-                'security.protocol': 'SASL_SSL',
-                'sasl.mechanisms': 'PLAIN',
-                'sasl.username': 'IHO7XVPCJCCBZAYX',
-                'sasl.password': 'UAwjmSIn5xuAL7HZmBjU4NGt0nLfXbyjtlVA7imgCdGBYFkog5kw0gc4e5MYmiUE'
-            })
-
-            KAFKA_TOPIC = "CustomerUpdate"
-
-            registry_client = SchemaRegistry(
-                "https://psrc-gq7pv.westus2.azure.confluent.cloud",
-                HTTPBasicAuth("MYXDIGGTQEEMLDU2", "azvNIgZyA4TAaOmCLzxvrXqDpaC+lamOvkGm2B7mdYrq9AwKl4IQuUq9Q6WXOp8U"),
-                headers={"Content-Type": "application/vnd.schemaregistry.v1+json"}
-            )
-
-            avroSerde = AvroKeyValueSerde(registry_client, KAFKA_TOPIC)
-            """
+            now = datetime.datetime.now()
 
             message = dict(
-                ID=456,
-                CUSTOMEREMAIL='sagittis.placerat@hotmail.edu',
-                TERM='20y',
-                TYPE='life',
-                NAME='life policy 123 20y ',
-                DESCRIPTION='test description',
-                CURRENCY='HKD',
-                PREMIUMPAYMENT='monthly',
-                PREMIUMSTRUCTURE='premium structure',
-                STATUS='Draft'
+                POLICYTERM=uw_result['term'],
+                POLICYTYPE=uw_result['ctype'],
+                POLICYNAME=uw_result['name'],
+                POLICYDESCRIPTION=uw_result['desc'],
+                POLICYCURRENCY='HKD',
+                PREMIUMPAYMENT=uw_result['premiumpayment'],
+                PREMIUMSTRUCTURE=uw_result['premiumstructure'],
+                POLICYSTATUS=status['policy_status'],
+                REASON=uw_result['reason'],
+                UWTIME=now.strftime("%Y-%m-%d:%H"),
+                CUSTOMERNAME=uw_result['cust_name'],
+                GENDER=uw_result['gender'],
+                DOB=uw_result['dob'],
+                COUNTRY=uw_result['country'],
+                EMAIL=uw_result['email'],
+                SMOKING_STATUS=uw_result['smoking_status'],
+                CUSTOMER_STATUS=status['customer_status']
             )
+            key = uw_result['policyid']
 
             sr = SchemaRegistryClient({
                 "url": "https://psrc-gq7pv.westus2.azure.confluent.cloud",
                 "basic.auth.user.info": "MYXDIGGTQEEMLDU2:azvNIgZyA4TAaOmCLzxvrXqDpaC+lamOvkGm2B7mdYrq9AwKl4IQuUq9Q6WXOp8U"
             })
 
-            """
-            producer.produce(topic, key=1, value=avroSerde.value.serialize(message, value_schema), callback=acked)
-            producer.flush()
-            """
             path = os.path.realpath(os.path.dirname(__file__))
             with open(f"{path}/policy_schema.avsc") as f:
                 schema_str = f.read()
-            #print(schema_str)
 
             avro_serializer = AvroSerializer(sr, schema_str)
-            #SerializingProducer
+
             producer = SerializingProducer({
                     'bootstrap.servers': 'pkc-epwny.eastus.azure.confluent.cloud:9092',
                     'security.protocol': 'SASL_SSL',
                     'sasl.mechanisms': 'PLAIN',
                     'sasl.username': 'IHO7XVPCJCCBZAYX',
                     'sasl.password': 'UAwjmSIn5xuAL7HZmBjU4NGt0nLfXbyjtlVA7imgCdGBYFkog5kw0gc4e5MYmiUE',
-                    'key.serializer': StringSerializer('utf_8'),
+                    'key.serializer': IntegerSerializer,
                     'value.serializer': avro_serializer
                 })
 
-            producer.produce(topic='Policy', key='', value=message, on_delivery=acked)
+            producer.produce(topic='PolicyUWResult', key=key, value=message, on_delivery=acked)
             producer.poll(0)
             producer.flush()
 
-        if 'Accept' in request.form: 
+        #uw_result = None
+        # Get UW result data
+        print(request.form.get('premiumstructure'))
+        uw_result = {
+            'term': str(request.form.get('term')) + 'y',
+            'premiumpayment': request.form.get('premiumpayment'),
+            'email': request.form.get('email'),
+            'premiumstructure': request.form.get('premiumstructure'),
+            'desc': request.form.get('desc'),
+            'ctype': request.form.get('ctype'),
+            'name': request.form.get('name'),
+            'reason': request.form.get('reason'),
+            'cust_id': request.form.get('cust_id'),
+            'cust_name': request.form.get('cust_name'),
+            'gender': request.form.get('gender'),
+            'dob': request.form.get('dob'),
+            'country': request.form.get('country'),
+            'smoking_status': request.form.get('smoking_status'),
+            'policy_id': request.form.get('policy_id')
+        }
+        print(uw_result)
 
-            kwargs = {
-            'term': POLICYTERM + 'y',
-            'premiumpayment': PREMIUMSTRUCTURE,
-            'email': EMAIL,
-            'premiumstructure': PREMIUMPAYMENT,
-            'desc': POLICYDESCRIPTION,
-            'ctype': POLICYTYPE,
-            'name': CUSTOMERNAME,
-            'cus_id': CUSTOMERID
+        if 'Accept' in request.form:
+            # If policy is approved, Prospect become a Customer
+            status = {
+                'policy_status': 'Approved',
+                'customer_status': True
             }
-            prod = producer(**kwargs)
+            #producer(uw_result, status)
 
             return render_template("accept.html")
             
         if 'Decline' in request.form:
-            kwargs = {
-            'term': POLICYTERM+'y',
-            'premiumpayment': PREMIUMSTRUCTURE,
-            'email': EMAIL,
-            'premiumstructure': PREMIUMPAYMENT,
-            'desc': POLICYDESCRIPTION,
-            'ctype': POLICYTYPE,
-            'name': CUSTOMERNAME,
-            'cus_id': CUSTOMERID
+            status = {
+                'policy_status': 'Declined',
+                'customer_status': request.form.get('customer_status')
             }
-            prod = producer(**kwargs)
+            #producer(uw_result, status)
+
             return render_template("decline.html")
 
     if request.method == "GET":
@@ -310,6 +287,7 @@ def index():
                             return(v)
             finally:
                 consumer.close()
+
         def client_consumed():
             # topic name used by parser
             KAFKA_TOPIC = "PolicyDraftList"
@@ -329,7 +307,8 @@ def index():
             )
             avroSerde = AvroValueSerde(registry_client, KAFKA_TOPIC)
             return basic_consume_loop(consumer, ["PolicyDraftList"], avroSerde)
-        
+
+
         # Store the dict from the consumer call
         v = client_consumed()
         print(v)
@@ -346,34 +325,20 @@ def index():
         POLICYSTATUS = v['POLICYSTATUS']
         COUNTRY = v['COUNTRY']
         EMAIL = v['EMAIL']
-        g = v['CUSTOMER_STATUS']
-        h = v['SMOKING_STATUS']
-        k = v['CUSTOMER_STATUS']
+        CUSTOMER_STATUS = 'Yes' if v['CUSTOMER_STATUS'] else 'No'
+        SMOKING_STATUS = 'Yes' if v['SMOKING_STATUS'] else 'No'
         POLICYTERM = v['POLICYTERM']
-        POLICYDESCRIPTION = v['POLICYDESCRIPTION']
         DOB = v['DOB']
-        if g == True:
-            SMOKING_STATUS = 'Yes'
-        else: 
-            SMOKING_STATUS = 'No'
-        if h == True:
-            SMOKING_STATUS = 'Yes'
-        else: 
-            SMOKING_STATUS = 'No'
-        if v == True:
-            CUSTOMER_STATUS = 'Yes'
-        else:
-            CUSTOMER_STATUS = 'No'
 
         return render_template("index.html", SMOKING_STATUS=SMOKING_STATUS,
-        CUSTOMER_STATUS=CUSTOMER_STATUS,EMAIL=EMAIL, COUNTRY=COUNTRY,
-        POLICYSTATUS=POLICYSTATUS,CUSTOMERNAME=CUSTOMERNAME,GENDER=GENDER,
-        PREMIUMSTRUCTURE=PREMIUMSTRUCTURE,PREMIUMPAYMENT=PREMIUMPAYMENT,
-        POLICYCURRENCY=POLICYCURRENCY, POLICYDESCRIPTION=POLICYDESCRIPTION, 
-        POLICYNAME=POLICYNAME, POLICYTYPE=POLICYTYPE, CUSTOMERID=CUSTOMERID,
-        POLICYTERM=POLICYTERM, DOB=DOB)
-        
-        
+            CUSTOMER_STATUS=CUSTOMER_STATUS, EMAIL=EMAIL, COUNTRY=COUNTRY,
+            POLICYSTATUS=POLICYSTATUS, CUSTOMERNAME=CUSTOMERNAME, GENDER=GENDER,
+            PREMIUMSTRUCTURE=PREMIUMSTRUCTURE, PREMIUMPAYMENT=PREMIUMPAYMENT,
+            POLICYCURRENCY=POLICYCURRENCY, POLICYDESCRIPTION=POLICYDESCRIPTION,
+            POLICYNAME=POLICYNAME, POLICYTYPE=POLICYTYPE, CUSTOMERID=CUSTOMERID,
+            POLICYTERM=POLICYTERM, DOB=DOB)
+
+
 def errorhandler(e):
     """Handle error"""
     if not isinstance(e, HTTPException):
